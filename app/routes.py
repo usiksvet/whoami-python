8ea571fdff44f917fda25e0f8d6390ccef846b5a
Martin Kulich <martin.kulich@tatum.io>
2024-08-17 14:14:44 +0200

    Render map

--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,41 @@
 import datetime
+import folium
 import ipaddress
 import requests
 
 from app import app, db
 from app.models import Visitor
-from flask import request
+from flask import request, render_template
 from sqlalchemy import func, desc
 
 
 @app.route('/')
 def index():
-    ip_address = request.remote_addr
+    remote_addr = request.remote_addr
 
-    if not store_ip_address(ip_address):
+    if not store_ip_address(remote_addr):
         return "Error retrieving location data", 500
 
     visitors = (
         db.session.query(
             Visitor.ip_address,
             func.max(Visitor.timestamp).label('last_seen'),
-            func.count(Visitor.id).label('count')
+            func.count(Visitor.id).label('count'),
+            Visitor.longitude,
+            Visitor.latitude,
         )
         .group_by(Visitor.ip_address)
         .order_by(desc('last_seen'))
         .all()
     )
-    # map = folium.Map(location=[0, 0], zoom_start=2)
-    # for visitor in visitors:
-    #     folium.Marker(location=[visitor.latitude, visitor.longitude]).add_to(map)
-    # map_html = map._repr_html_()
 
-    # return render_template('index.html', map_html=map_html)
+    map = folium.Map(location=[0, 0], zoom_start=2)
+    for visitor in visitors:
+        if visitor.latitude and visitor.longitude:
+            folium.Marker(location=[visitor.latitude, visitor.longitude]).add_to(map)
+    map_html = map._repr_html_()
 
-    return [f"IP Address: {v.ip_address}, Last Seen: {v.last_seen}, Count: {v.count}"
-            for v in visitors]
+    return render_template('index.html', map_html=map_html, visitors=visitors)
 
 
 @app.route('/ip')
@@ -60,9 +62,9 @@ def store_ip_address(addr):
             ip_address=addr,
         )
     else:
-        data = requests.get(f'http://ip-api.com/json/{ip_address}').json()
+        data = requests.get(f'http://ip-api.com/json/{addr}').json()
 
-        if data['status'] == 'success':
+        if data['status'] != 'success':
             return False
 
         visitor = Visitor(
