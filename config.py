2eb25d9c10bf5e19267afdee837891d6a9c6ad22
Martin Kulich <kulda.zbraslav@gmail.com>
2025-01-18 15:38:30 +0100

    config descriptions

--- config.py
+++ config.py
@@ -3,5 +3,9 @@ import os
 class Config:
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') if os.environ.get('DATABASE_URL') else 'sqlite:///:memory:'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  
+  # Map center coordinates
   MAP_CENTER = [0, 0]
+
+  # Default map zoom, range 0-18
   MAP_ZOOM = 2
